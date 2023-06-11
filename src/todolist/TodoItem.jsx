b361bca51adb75b4a3d348c2a5c9b8ae07765c48
Hassan Ahmed Khan <118908309+hassanahmedkhan9015@users.noreply.github.com>
2023-06-12 01:56:42 +0500

    Add files via upload

---
+++ src/todolist/TodoItem.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import DeleteIcon from "../assets/deleteicon.png"
+import CheckIcon from "../assets/editicon.png"
+import "./todo.css"
+
+export const TodoItem = ({title, deleteTodo}) =>{
+  const [done, setDone] = useState(false);
+  const whenClicked = () => {
+    setDone(!done);
+  };
+  return (
+    <>
+    <div className="todo-item" id="todo-1">
+      <p className="todo-task-title" style={{ backgroundColor: done ? "#fc5656" : "white" }}>{title}</p>
+      <img className="todo-icon" src={DeleteIcon} onClick={deleteTodo} />
+    </div>
+      <img className="todo-one-icon" src={CheckIcon} onClick={whenClicked}/>
+    </>
+  )
+}
